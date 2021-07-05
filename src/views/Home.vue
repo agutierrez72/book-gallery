346a017c9c522eb3f812ed4aaf16b16e78d4c9a8
Alina <agutierrez72@ucmerced.edu>
2021-07-05 15:24:20 -0700

    import SingleBook component, display content in db.json

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,68 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+      <!-- <div v-for="book in books" :key="book.id">  
+        <SingleBook :book="book" />
+      </div> -->
+    <swiper
+      :slides-per-view="3"
+      :space-between="50"
+      navigation
+      :pagination="{ clickable: true }"
+      @swiper="onSwiper"
+      @slideChange="onSlideChange"
+    >
+      <swiper-slide v-for="book in books" :key="book.id"><SingleBook :book="book"/></swiper-slide>
+
+      ...
+    </swiper>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import {ref} from 'vue'
+import SingleBook from '../components/SingleBook.vue'
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import {Swiper, SwiperSlide} from 'swiper/vue'
+import 'swiper/swiper-bundle.css'
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    SingleBook, Swiper, SwiperSlide
+
+  },
+  methods: {
+    onSwiper(swiper) {
+      console.log(swiper);
+    },
+    onSlideChange() {
+      console.log('slide change');
+    },
+  },
+  setup(){
+    const books = ref([])
+    const error = ref(null)
+
+    const load = async () => {
+      try {
+        let data = await fetch('http://localhost:3000/books')
+        console.log(data)
+        if(!data.ok){
+          throw Error('no data retrieved')
+        }
+        books.value = await data.json()
+      } catch (err) {
+        error.value = err.message
+      }
+    }
+
+    load()
+
+    return { books }
   }
 }
 </script>
