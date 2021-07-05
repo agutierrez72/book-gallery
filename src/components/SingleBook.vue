438d24a5f1c110b7544564ca329a6de9682e53bd
Alina <agutierrez72@ucmerced.edu>
2021-07-05 15:23:38 -0700

    create singleBook component

---
+++ src/components/SingleBook.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="book">
+    <img class=" image h-28 place-self-center" :src="imageSrc">
+    <h2>{{ book.title }}</h2>
+    <h4>{{book.author}}</h4>
+    <p>{{ book.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['book'],
+    computed: {
+        imageSrc(){
+            return require('../assets/'+this.book.image)
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
